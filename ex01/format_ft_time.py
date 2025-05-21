67225fca9935aeaf5cf8daef9084652d63196549
Nauman Munir <nmunir@lab1r4s9.42abudhabi.ae>
2025-05-21 18:14:20 +0400

    - ex01 done. Learn about time in python.

---
+++ ex01/format_ft_time.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from time import mktime
+
+
+
+now = datetime.now()
+microSeconds = now.microsecond
+
+seconds = mktime(now.timetuple()) + microSeconds / 1_000_000
+
+
+# timeNow = time.time()
+
+print(f"Seconds since January 1, 1970: {seconds:.4f} or {seconds:.2e} in scientific notation")
+print(now.strftime("%b %d %Y"))
