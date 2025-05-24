87ffc1420f5ad34a5e8f608bda16ae603328ef3b
Nauman Munir <nmunir@lab1r4s9.42abudhabi.ae>
2025-05-24 13:44:54 +0400

    - ex05 finished. - Learned about function variables scoping and baseException.

--- ex05/building.py
+++ ex05/building.py
@@ -8,22 +8,22 @@ def main():
     if len(argv) == 1:
         try:
             text = input("What is the text to count?\n")
-        except EOFError:
+        except BaseException:
             return
     else:
         assert len(argv) == 2, "more than one argument is provided"
-    text = argv[1]
+        text = argv[1]
 
     upper = sum(1 for c in text if c.isupper())
     lower = sum(1 for c in text if c.islower())
     digits = sum(1 for c in text if c.isdigit())
     spaces = sum(1 for c in text if c.isspace())
     punct = sum(1 for c in text if c in punctuation)
-    print(f"Uppercase letters: {upper}")
-    print(f"Lowercase letters: {lower}")
-    print(f"Digits: {digits}")
-    print(f"Spaces: {spaces}")
-    print(f"Punctuation: {punct}")
+    print(f"{upper} upper letters")
+    print(f"{lower} lower letters")
+    print(f"{punct} punctuation")
+    print(f"{spaces} spaces")
+    print(f"{digits} digits")
 
 
 if __name__ == "__main__":
