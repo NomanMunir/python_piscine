7ab52b27b51b18cf9cb828ea99bb42d0b583f5f0
Nauman Munir <mrnomanmunir@gmail.com>
2025-08-21 19:10:54 +0400

    feat: Implement Character, Baratheon, and Lannister classes with die method and string representation

---
+++ 03_python_oop/ex01/S1E7.py
@@ -0,0 +1,57 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+    """Representing the House Baratheon."""
+
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """Initialize a Baratheon character."""
+        super().__init__(first_name, is_alive)
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+
+    def die(self):
+        """Kill the Baratheon character."""
+        self.is_alive = False
+
+    def __str__(self):
+        """Return string representation of Baratheon character."""
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def __repr__(self):
+        """Return detailed string representation of Baratheon character."""
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    @classmethod
+    def create_lannister(cls, first_name: str, is_alive: bool = True):
+        """Create a Lannister character from Baratheon class."""
+        return Lannister(first_name, is_alive)
+
+
+class Lannister(Character):
+    """Representing the House Lannister."""
+
+    def __init__(self, first_name: str, is_alive: bool = True):
+        """Initialize a Lannister character."""
+        super().__init__(first_name, is_alive)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+
+    def die(self):
+        """Kill the Lannister character."""
+        self.is_alive = False
+
+    def __str__(self):
+        """Return string representation of Lannister character."""
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def __repr__(self):
+        """Return detailed string representation of Lannister character."""
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    @classmethod
+    def create_lannister(cls, first_name: str, is_alive: bool = True):
+        """Create a Lannister character from Lannister class."""
+        return cls(first_name, is_alive)
